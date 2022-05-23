fe932606c332a6ca6a2a8582a080fa3b5d426baa
AmyFong82 <amyhfong@gmail.com>
2022-05-23 11:54:35 -0400

    Added Select options to the form of task status.

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,10 @@ class Task < ApplicationRecord
   validates :status, inclusion: { in: [
     'not-started', 'in-progress', 'complete'
   ]}
+
+  STATUS_OPTIONS = [
+    ['Not Started', 'not-started'],
+    ['In Progress', 'in-progress'],
+    ['Complete', 'complete']
+  ]
 end
